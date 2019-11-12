8ef8f08fba21680b6003249b792c41fa106a461a
mariedpy <marie.dpy@hotmail.com>
2019-11-12 20:13:08 +0100

    commit_js

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+var counterElement = document.getElementById("counter-element-js");
+
+var counter = 3;
+
+counterElement.textContent = counter;
+
+var countdown = setInterval(function(){
+  if (counter > 0) {
+    counter = counter - 1;
+    counterElement.textContent = counter;
+  } else {
+    clearInterval(countdown);
+    history.pushState({}, "", "intro_jeu.html")
+  }
+}, 1000);
